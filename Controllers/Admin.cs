fbd02c039408c336143b215476ce8db4c9ab360a
mayur <mostact@gmail.com>
2023-12-10 22:32:54 +0530

    api

---
+++ Controllers/Admin.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NGOAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text.Json;
+namespace NGOAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Admin : ControllerBase
+    {
+        [HttpPost("SendEmail")]
+        public IActionResult SendEmail(ContactForm _ContactForm)
+        {
+            MailAddress to = new MailAddress(_ContactForm.Email);
+            MailAddress from = new MailAddress("mostact@gmail.com");
+
+            MailMessage email = new MailMessage(from, to);
+            email.Subject = "Thank you for visiting Dreams Peace Foundation";
+            email.Body = _ContactForm.Message;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("mostact@gmail.com", "umco ghbe litj cyiu");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.EnableSsl = true;
+            Result _Result = new Result();
+            try
+            {
+
+                smtp.Send(email);
+                _Result.Status = "1";
+            }
+            catch (SmtpException ex)
+            {
+                _Result.Status = "0";
+                _Result.Data = ex;
+            }
+            return Content(JsonSerializer.Serialize<Result>(_Result).ToString(), "application/json");
+        }
+    }
+}
---
+++ Models/ContactForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGOAPI.Models
+{
+    public class ContactForm
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Message { get; set; }
+
+    }
+    public class Result
+    {
+        public string Status { get; set; }
+        public dynamic Data { get; set; }
+    }
+
+}
